865372482f8b2c992eff0e6b25aa4fa0d557d61f
zhanghao <askldfhjg@gmail.com>
2012-11-06 21:02:21 +0800

    omMessage

--- event.js
+++ event.js
@@ -1,19 +1,20 @@
-if(!chrome.webNavigation.onBeforeNavigate.hasListeners()) {
-	chrome.webNavigation.onBeforeNavigate.addListener(function(detail){
-		console.log(detail);
-		chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-			if (request.msg == "data") {
-				chrome.storage.sync.get("css", function(obj){
-					console.log(obj["css"]);
-					sendResponse({farewell: obj["css"]});
-					return true;
-				});
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
+	if (request.msg) {
+		chrome.storage.sync.get("css", function(obj){
+			console.log(obj["css"]);
+			var result = obj["css"].replace(/\n/ig, "|").split('|');
+			var found = false;
+			for(var i = 0;i < result.length; i++) {
+				var reg = result[i].replace(/\*/ig, "[a-zA-Z0-9_\-]*");
+				var re = new RegExp(reg, "ig");
+				if(re.exec(request.msg)) {
+					found = true;
+				}
 			}
-			else {
-				sendResponse({});
-				return true;
+			if(found) {
+				sendResponse({farewell: obj["css"]});
 			}
-			return true;
 		});
-	});
-}
+	}
+	return true;
+});
--- iframe.js
+++ iframe.js
@@ -1,26 +1,27 @@
 var re = /Lbjs.TargetUrl\s+=\s*['"]([a-zA-Z:\/.0-9_]+)['"];/i
 var url = document.location.href;
-chrome.extension.sendMessage({msg: "data"}, function(response){
-  console.log("eee");
-  console.log(response.farewell);return;
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      var arr = re.exec(xhr.responseText);
-      if(arr != null)
-      {
-        var target = arr[1];
-        console.log(target);
-        //var body = document.getElementsByTagName("body");
-        //body = body[0];
-        //var div = document.createElement("div");
-        //div.setAttribute("style", "position:fixed;width:200px;height:200px;background-color:black;top: 0px;");
-        //div.setAttribute("onclick","alert('" +target +" ')");
-        //body.appendChild(div);
-        window.open(target);
+chrome.extension.sendMessage({msg: url}, function(response){
+  console.log(response.farewell);
+  if(response.farewell) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        var arr = re.exec(xhr.responseText);
+        if(arr != null)
+        {
+          var target = arr[1];
+          console.log(target);
+          //var body = document.getElementsByTagName("body");
+          //body = body[0];
+          //var div = document.createElement("div");
+          //div.setAttribute("style", "position:fixed;width:200px;height:200px;background-color:black;top: 0px;");
+          //div.setAttribute("onclick","alert('" +target +" ')");
+          //body.appendChild(div);
+          window.open(target);
+        }
       }
     }
+    xhr.send();
   }
-  xhr.send();
 });
